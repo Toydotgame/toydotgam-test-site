77e85745eed9d8b725647c066902c3c038ca28dc
Toydotgam <MatthewJPersonalAcct@outlook.com>
2020-05-08 11:39:15 +1000

    Revert "Redo's Site Commit"
    This reverts commit 5627526f18d66c62cfeae4eee673df3e9807de74.

--- passwordTest/passwordScript.js
+++
@@ -1,9 +0,0 @@
-function loginPrompt() {
-    var password = prompt("Please enter your staff code:");
-    if (password == "string") {
-        alert("Correct!")
-    }
-    else {
-        alert("the correct one is \"string\"")
-    }
-}
